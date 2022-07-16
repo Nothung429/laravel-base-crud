20dabdc7075b7010f1e8298ea4dccde6d36bc78b
Nothung429 <frank.gatto96.fg@gmail.com>
2022-07-16 17:38:25 +0200

    fix

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,10 +9,10 @@
     </head>
 
     <body>
-        <header>        
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container">
-                    <a class="navbar-brand" href="#">DC Comix</a>
+                    <a class="navbar-brand pe-5" href="#">DC Comix</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
